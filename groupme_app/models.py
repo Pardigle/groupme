44baec1247c2bb26eb806c4294c2a42c4bd9214d
vernal <vernalterrene@gmail.com>
2025-10-27 22:54:43 +0800

    turn classes into django models

--- groupme_app/models.py
+++ groupme_app/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-class User:
-    def __init__(self, username:str, displayName:str, contactDetails:str, timeslots:set):
-        self.username = username
-        self.displayName = displayName
-        self.contactDetails = contactDetails
-        self.userSchedule = timeslots
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    displayName = models.CharField(max_length=100)
+    contactDetails = models.CharField(max_length=100)
+    userSchedule = models.JSONField(default=set)
 
     def get_username(self):
         return self.username
@@ -13,13 +12,12 @@ class User:
     def get_schedule(self):
         return self.userSchedule
 
-class Section:
-    def __init__(self, name:str, passcode:str, details:str, size:int):
-        self.passcode = passcode
-        self.sectionName = name
-        self.sectionDetails = details
-        self.maxSize = size
-        self.studentList = []
+class Section(models.Model):
+    passcode = models.CharField(max_length=100)
+    sectionName = models.CharField(max_length=100)
+    sectionDetails = models.CharField(max_length=200)
+    maxSize = models.IntegerField(default=2)
+    studentList = models.ManyToManyField(User)
 
     def get_studentList(self):
         return self.studentList
@@ -34,8 +32,14 @@ class Section:
         else:
             print("CLass is full")
 
+# References:
+# MANYTOMANYFIELD: https://docs.djangoproject.com/en/5.2/topics/db/examples/many_to_many/
+# JSONFIELD: https://docs.djangoproject.com/en/5.2/ref/models/fields
 
 
+
+# PLACING THE SIMILAR HOUR FINDING/SORTING HERE FOR NOW
+'''
 def find_similar_hours(currentUser:User, currentSection:Section):
   similarHours = [] #total number of similar free hours per student. key is username, value is no. of free hours
   rankingList = currentSection.get_studentList().copy()
@@ -84,9 +88,9 @@ def rank_schedules(similarHours): #mergesorted
     firstHalf = rank_schedules(firstHalf)
     secondHalf = rank_schedules(secondHalf)
     return merge(firstHalf, secondHalf)
+'''
 
-
-
+# TEST RUN
 '''
 a = User("userA", "dnA", "123A", {"M-1100-1130", "M-1130-1200", "M-1200-1230", "TH-1530-1600"})
 b = User("userB", "dnB", "234B", {"M-1100-1130", "M-1130-1200", "M-1200-1230", "TH-1530-1600", "F-0930-1100"})
--- groupme_app/views.py
+++ groupme_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import User, Section
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'groupme_app/homepage.html')
