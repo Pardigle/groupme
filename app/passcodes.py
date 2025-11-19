c8849db8a5096bf893670df54b29e47e5012bc0e
pardige <140949466+Pardigle@users.noreply.github.com>
2025-11-19 21:16:48 +0800

    Added create_passcode docstring and updated requirements.txt

--- app/passcodes.py
+++ app/passcodes.py
@@ -1,14 +1,14 @@
 import secrets
 import string
 
-'''
-Passcode implementation inspired by the following stack overflow answer: https://stackoverflow.com/a/39596292
-'''
-
 ALLOWED_TYPES = string.ascii_uppercase + string.digits
 PASSCODE_LENGTH = 6
 
 def create_passcode() -> str:
+    """Creates an ASCII and digit passcode.
+
+    Passcode implementation inspired by the following stack overflow answer: https://stackoverflow.com/a/39596292
+    """
     chars = [secrets.choice(ALLOWED_TYPES) 
              for i in range(PASSCODE_LENGTH)]
     passcode = ''.join(chars)
